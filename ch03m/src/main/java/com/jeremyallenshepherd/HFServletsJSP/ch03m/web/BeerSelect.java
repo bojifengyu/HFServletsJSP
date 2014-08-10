fb466bf3757e9d5cd891d8fc8e09babc7e20136c
Jeremy Shepherd <jeremy.shepherd@outlook.com>
2014-08-10 15:47:47 -0400

    optimized imports

--- ch03m/src/main/java/com/jeremyallenshepherd/HFServletsJSP/ch03m/web/BeerSelect.java
+++ ch03m/src/main/java/com/jeremyallenshepherd/HFServletsJSP/ch03m/web/BeerSelect.java
@@ -1,12 +1,15 @@
 package com.jeremyallenshepherd.HFServletsJSP.ch03m.web;
 
-import com.jeremyallenshepherd.HFServletsJSP.ch03m.model.*;
+import java.io.IOException;
+import java.util.List;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-import java.io.*;
-import java.util.*;
+import com.jeremyallenshepherd.HFServletsJSP.ch03m.model.BeerExpert;
 
 /**
  * <p>
